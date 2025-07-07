3289877589be3c95e8efe0b7145b07700baa2f13
vivek-codepalette <vivek@codepalette.ai>
2025-07-08 00:06:45 +0700

    update fixes

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { MDXRemote } from 'next-mdx-remote-client/rsc';
 
-export default async function RemoteMdxPage({
+export default async function BlogPostPage({
   params,
 }: {
   params: Promise<{
@@ -9,18 +9,16 @@ export default async function RemoteMdxPage({
   }>;
 }) {
   const { slug } = await params;
-  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+  const res = await fetch(`${process.env.YAST_BLOG_API_URL}/posts/${slug}`, {
     headers: {
-      Authorization: `${process.env.YAST_BLOG_API_KEY}`,
+      Authorization: `Bearer ${process.env.YAST_BLOG_API_KEY}`,
     },
     method: 'GET',
   });
-  console.log(res);
   if (!res.ok) {
     return <div>Error loading post</div>;
   }
   const data = await res.json();
-  console.log(data);
   const post = data.data as {
     title: string;
     content: string;
@@ -31,6 +29,7 @@ export default async function RemoteMdxPage({
       <h1>{post.title}</h1>
       <MDXRemote
         components={{
+          // biome-ignore lint/nursery/noNestedComponentDefinitions: <explanation>
           Image: ({ src, alt }) => (
             <Image alt={alt} height={500} src={src} width={500} />
           ),
---
+++ middleware.ts
@@ -0,0 +1,37 @@
+import { after, type NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  // Forward the original request
+  const response = NextResponse.next();
+
+  after(async () => {
+    await fetch(`${process.env.YAST_BLOG_API_URL}/api/logs`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // Forward original request headers
+        ...Object.fromEntries(request.headers.entries()),
+      },
+      body: JSON.stringify({
+        url: request.url,
+        method: request.method,
+        timestamp: new Date().toISOString(),
+      }),
+    });
+  });
+
+  return response;
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
